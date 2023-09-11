7f126e268aca1e16f8f683b62a859b7965dc618b
mozihe <zhujunheng2005@163.com>
2023-09-11 09:02:04 +0800

    22
    11

---
+++ new.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(void)
+{
+    int N , a[999] , i , j , t;
+
+    printf("请输入数据容量");
+    scanf("%d" , &N);
+    printf("请依次输入数据\n");
+    for (i = 0 ; i < N ; i++)
+    {
+        scanf("%d" , &a[i]); 
+    }
+        
+    for (i = 0 ; i < N-1 ; i++)
+    {
+        for (j = i+1 ; j < N ; j++)
+        {
+            if (a[i] < a[j])
+            {
+                t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }  
+        }      
+    }
+    
+    printf("排序结果为");    
+    for (i = 0 ; i < N ; i++)
+    {
+        printf("%d " , a[i]);
+    }
+        
+    return 0;
+}
